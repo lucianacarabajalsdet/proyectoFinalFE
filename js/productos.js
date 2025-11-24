833eca9a935085efcf897cf17b77b6e64d78aa06
Luciana Carabajal <luciana.carabajal@globallogic.com>
2025-11-23 23:25:18 -0300

    commit inicial

---
+++ js/productos.js
@@ -0,0 +1,8 @@
+//Usar rutas ABSOLUTAS ya que ahora se usaran desde 2 html en distintas ubicaciones
+export const productos = [
+  { id: "1", nombre: "Sistema de Reservas y Agenda Online", img: "img/reservaCitaPage.png", precio: 200 },
+  { id: "2", nombre: "Tienda Online (E-commerce)", img: "img/eccomerceWebPage.png",          precio: 200 },
+  { id: "3", nombre: "Blog Personal", img: "img/blogPersonal.png",                       precio: 300 },
+  { id: "4", nombre: "Formularios Personalizados", img: "img/formulariosWeb.png",          precio: 150 },
+  { id: "5", nombre: "Catálogos Online", img: "img/catalogosOnline.png",                    precio: 150 },
+];
